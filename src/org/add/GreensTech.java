7bff7290f1eab02a39772949efb1f4cba10a57cf
ksks1711 <ksks171192@gmail.com>
2022-08-18 14:13:50 +0530

    FirstGit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,44 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		
+		System.out.println("GreensTech OMR : Plot No 19, Rajiv Gandhi Salai Thoraipakkam Chennai - 97");
+		}
+	
+	private void greensAdayar() {
+		
+		System.out.println("GreensTech Adayar : 10/15, 1st Street, Padmanabha Nagar, Adyar, Chennai - 20");
+	}
+	
+	private void greensTamabaram() {
+		
+		System.out.println("GreensTech Tambaram : 1 Apparao Colony, Tambaram Sanatorium, Chennai - 47");
+	}
+	
+	private void greensVelacherry() {
+		
+		System.out.println("GreensTech Velacherry : No 28, Nagendra Nagar, Velacherry, Chennai - 42");
+		}
+	
+	private void greensAnnanagar() {
+		
+		System.out.println("GreensTech Annanagar : 4th Floor, W122, 3rd Ave, W Block, Annanagar East, Chennai - 40");		
+	}
+
+	public static void main(String[] args) {
+		
+		GreensTech g = new GreensTech();
+				
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTamabaram();
+		g.greensVelacherry();
+		g.greensAnnanagar();
+		
+	}
+
+}
+
+
